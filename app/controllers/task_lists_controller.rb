af09882a7549361fbca91cc048201a1cb890dc3d
zskzsk92 <zsk1992on@gmail.com>
2023-01-13 18:05:29 +0900

    タスクリスト投稿機能

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -4,9 +4,22 @@ class TaskListsController < ApplicationController
   end
 
   def new
+    @task_list = TaskList.new
   end
 
   def create
+    @task_list = TaskList.new(task_list_params)
+    if @task_list.save
+      redirect_to :root
+    else
+      render :new
+    end
   end
-  
+
+  private
+
+  def task_list_params
+    params.require(:task_list).permit(:title).merge(user_id: current_user.id)
+  end
+
 end
